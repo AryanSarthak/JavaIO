d0051fa2066c8c5cbfbde5867086211985d8af7b
XEROX112 <jayabhardwaz1@gmail.com>
2025-12-27 19:39:15 +0530

    Reading from file

--- src/javaio/EmployeePayrollService.java
+++ src/javaio/EmployeePayrollService.java
@@ -1,11 +1,17 @@
 package javaio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    private List<EmployeePayroll> employeePayrollList;
+
+    private static final String FILE_NAME = "employee-payroll.txt";
+    final List<EmployeePayroll> employeePayrollList;
 
     public EmployeePayrollService() {
         employeePayrollList = new ArrayList<>();
@@ -27,5 +33,42 @@ public class EmployeePayrollService {
 
     public void writeEmployeePayrollData() {
         System.out.println("Employee Payroll Data: " + employeePayrollList);
+    }
+
+    public void writeEmployeePayrollToFile() {
+        StringBuilder buffer = new StringBuilder();
+        employeePayrollList.forEach(emp ->
+                buffer.append(emp.toString()).append("\n"));
+        try {
+            Files.write(Paths.get(FILE_NAME), buffer.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printEmployeePayrollFromFile() {
+        try {
+            Files.lines(Paths.get(FILE_NAME)).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long countEntries() {
+        try {
+            return Files.lines(Paths.get(FILE_NAME)).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<String> readEmployeePayrollFromFile() {
+        try {
+            return Files.readAllLines(Paths.get(FILE_NAME));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 }
--- src/javaio/Main.java
+++ src/javaio/Main.java
@@ -1,13 +1,18 @@
 package javaio;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Java IO");
+
         EmployeePayrollService service = new EmployeePayrollService();
-        Scanner scanner = new Scanner(System.in);
-        service.readEmployeePayrollData(scanner);
-        service.writeEmployeePayrollData();
+
+        service.employeePayrollList.add(new EmployeePayroll(1, "Aryan", 50000));
+        service.employeePayrollList.add(new EmployeePayroll(2, "Rahul", 60000));
+        service.employeePayrollList.add(new EmployeePayroll(3, "Neha", 55000));
+
+        service.writeEmployeePayrollToFile();
+        service.printEmployeePayrollFromFile();
+        System.out.println(service.countEntries());
     }
 }
